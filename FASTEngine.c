0531780db49df9fe99d264b9c3acb0d9215dd130
Caio Oliveira <caiossoliveira@gmail.com>
2020-02-10 17:29:10 -0300

    bin file read implemented

--- FASTEngine.c
+++ FASTEngine.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+ 
+FILE* openFile(char* fileName);
+ 
+/* main function to call above defined function */
+int main () {
+	unsigned char byte;
+	//__int8_t byte_aux = 0b10000000;
+	unsigned int byte_aux = 128;
 
-int main(){
-	return 0;
+	FILE* file = openFile("51_Inc_FAST.bin");
+
+	//while(fread(&byte, 1, 1, file) > 0){
+	for(int i=0; i < 10; i++){
+		fread(&byte, 1, 1, file);
+
+		printf("%x ", byte);
+
+		if((byte - byte_aux) <= 128){ //if( byte AND 10000000) >= 10000000 : means the first bit is 1.
+			printf("It is the last byte. \n"); //- Byte: %d Byte_aux: %d \n", byte, byte_aux);
+		}
+		else{
+			printf("It is not the last byte. \n");
+		}
+	}
+
+   return 0;
+}
+
+FILE* openFile(char* fileName) {
+   static FILE* file;
+   file = fopen(fileName, "rb"); 
+   return file;
 }
