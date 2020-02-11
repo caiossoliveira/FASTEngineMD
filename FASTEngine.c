5a2f206da324b269f3b95f56ffac960050fcc432
Caio Oliveira <caiossoliveira@gmail.com>
2020-02-11 14:56:30 -0300

    shift implemented to identify the last byte of each field

--- FASTEngine.c
+++ FASTEngine.c
@@ -2,25 +2,20 @@
  
 FILE* openFile(char* fileName);
  
-/* main function to call above defined function */
 int main () {
+
 	unsigned char byte;
-	//__int8_t byte_aux = 0b10000000;
-	unsigned int byte_aux = 128;
+	__uint8_t byte_aux = 0b10000000;
 
 	FILE* file = openFile("51_Inc_FAST.bin");
 
-	//while(fread(&byte, 1, 1, file) > 0){
-	for(int i=0; i < 10; i++){
-		fread(&byte, 1, 1, file);
-
+	while(fread(&byte, 1, 1, file) > 0){
 		printf("%x ", byte);
-
-		if((byte - byte_aux) <= 128){ //if( byte AND 10000000) >= 10000000 : means the first bit is 1.
-			printf("It is the last byte. \n"); //- Byte: %d Byte_aux: %d \n", byte, byte_aux);
+		if((byte >> 7 && byte_aux >> 7)){
+			printf("Last byte of the field \n");
 		}
 		else{
-			printf("It is not the last byte. \n");
+			printf("Do not last byte of the field \n");
 		}
 	}
 
