475e6a2667fe73a70d5ca6d61bfdc21ab48840df
Caio Oliveira <caiossoliveira@gmail.com>
2020-02-19 17:46:15 -0300

    readField function improved to readMessage function

--- FASTEngine.c
+++ FASTEngine.c
@@ -2,110 +2,67 @@
 #include <string.h>
  
 FILE* openFile(char* fileName);
-void readField(FILE* file);
+void readMessage(FILE* file);
 void identifyField(__uint8_t* field, unsigned int field_length);
 void test();
  
 int main () {
-	readField(openFile("51_Inc_FAST.bin"));
+	readMessage(openFile("51_Inc_FAST.bin"));
 
     return 0;
 }
 
 void identifyField(__uint8_t* field, unsigned int field_length){
-	unsigned int status_MsgSeqNum;
-	__uint32_t MsgSeqNum;
 
 	for(int i=0; i < field_length; i++){
-		printf("%02x ", (unsigned int) field[i]); //%u to a series of bytes while(*field){printf("%02x ", (unsigned int) *field++); // cast the character to an unsigned type to be safe
+		printf(" %02x ", (unsigned int) field[i]); //%u to a series of bytes while(*field){printf("%02x ", (unsigned int) *field++); // cast the character to an unsigned type to be safe
 	}
 	printf("\n");
 }
 
-void readField(FILE* file){
+void readMessage(FILE* file){
+	__uint8_t header[10];
 	__uint8_t byte_aux = 0b10000000;
 	__uint8_t byte;
 	__uint8_t field[70000]; //2 bytes of MsgLength is the limit
 	unsigned int field_length = 0;
-
-	int status_MsgSeqNum = 0; //false
-	int status_NoChunks = 0;
-	int status_CurrentChunk = 0;
-	int status_MsgLength = 0;
-
-	int aux_MsgSeqNum[4];
-	int aux_NoChuncks[2];
-	int aux_CurrentChunk[2];
-	int aux_MsgLength[2];
-
 	int MsgSeqNum = 0;
 	int NoChunks = 0;
 	int CurrentChunk = 0;
 	int MsgLength = 0;
 
 	//while(fread(&byte, 1, 1, file) > 0){
-	for(int i = 0; i < 1; i++){ //24
-
-		if(status_MsgSeqNum == 0){ //getMsgSeqNum
-			for(int i = 0; i < 4; i++){
-				fread(&byte, 1, 1, file);
-				aux_MsgSeqNum[i] = byte;
-			}
-			MsgSeqNum = (aux_MsgSeqNum[0] << 24) | (aux_MsgSeqNum[1] << 16) | (aux_MsgSeqNum[2] << 8) | (aux_MsgSeqNum[3]); //concat the 4 bytes
-			printf("MsqSeqNum: %d \n", MsgSeqNum);
-			status_MsgSeqNum = 1; //true
+	for(int i = 0; i < 1; i++){ // number of messages
+		for(int i = 0; i < 10; i++){ //read header
+			fread(&byte, 1, 1, file);
+			header[i] = byte;
 		}
-		if(status_NoChunks == 0){
-			for(int i = 0; i < 2; i++){
-				fread(&byte, 1, 1, file);
-				aux_NoChuncks[i] = byte;
-			}
-			NoChunks = (aux_NoChuncks[0] << 8) | (aux_NoChuncks[1]);
-			printf("NoChuncks: %d \n", NoChunks);
-			status_NoChunks = 1; //true
-		}
-		if(status_CurrentChunk == 0){
-			for(int i = 0; i < 2; i++){
-				fread(&byte, 1, 1, file);
-				aux_CurrentChunk[1] = byte;
-			}
-			CurrentChunk = (aux_CurrentChunk[0] << 8) | (aux_CurrentChunk[1]);
-			printf("CurrentChunk: %d \n", CurrentChunk);
-			status_CurrentChunk = 1;
-		}
+		MsgSeqNum = (header[0] << 24) | (header[1] << 16) | (header[2] << 8) | (header[3]); //concatenate the bytes
+		NoChunks = (header[4] << 8) | (header[5]);
+		CurrentChunk = (header[6] << 8) | (header[7]);
+		MsgLength = (header[8] << 8) | (header[9]);
 
-		if(status_MsgLength == 0){
-			for(int i = 0; i < 2; i++){
-				fread(&byte, 1, 1, file);
-				aux_MsgLength[i] = byte;
-			}
-			MsgLength = (aux_MsgLength[0] << 8) | (aux_MsgLength[1]);
-			printf("MsgLength: %d \n", MsgLength);
-			status_MsgLength = 1;
-		}
+		printf(" MsqSeqNum: %d \n", MsgSeqNum);
+		printf(" NoChunks: %d \n", NoChunks);
+		printf(" CurrentChunk: %d \n", CurrentChunk);
+		printf(" MsgLength: %d \n", MsgLength);
+		printf(" Fields: \n");
 
-		if(status_MsgSeqNum == 1 && status_NoChunks == 1 && status_CurrentChunk == 1 && status_MsgLength == 1){ //if all the header was got
-			for(int i = 0; i < MsgLength; i++){
-				fread(&byte, 1, 1, file);
-				if(field_length == 0){
-					field[0] = byte;
-					field_length++;
-				}
-				else{
-					field[field_length] = byte;
-					field_length++;
-				}
-
-				if((byte >> 7 && byte_aux >> 7)){ // if the MSB is 1
-					identifyField(field, field_length);
-					strcpy(field, "");
-					field_length = 0;
-				}
+		for(int i = 0; i < MsgLength; i++){
+			fread(&byte, 1, 1, file);
+			if(field_length == 0){
+				field[0] = byte;
+				field_length++;
 			}
-			status_MsgSeqNum = 0; //clean all to get the next header
-			status_NoChunks = 0;
-			status_CurrentChunk = 0;
-			status_MsgLength = 0; 
+			else{
+				field[field_length] = byte;
+				field_length++;
+			}
+			if((byte >> 7 && byte_aux >> 7)){ // if the MSB is 1
+				identifyField(field, field_length);
+				strcpy(field, "");
+				field_length = 0;
+			}
 		}
 	}
 }
