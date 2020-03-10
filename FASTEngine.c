9c2c48f8ff2569eeedbce9566b815b9e0caa93f0
Caio Oliveira <caiossoliveira@gmail.com>
2020-03-09 21:55:56 -0300

    template 145 partially implemented

--- FASTEngine.c
+++ FASTEngine.c
@@ -7,6 +7,7 @@ void readMessage(FILE* file);
 void identifyField(__uint8_t* FASTMessage, unsigned int FASTMessage_length);
 void templateDecoder(__uint16_t TemplateID, __uint8_t* FASTMessage, unsigned int FASTMessage_length);
 void MDHeartbeat_144(__uint8_t* FASTMessage, unsigned int FASTMessage_length);
+void MDIncRefresh_145(__uint8_t* FASTMessage, unsigned int FASTMessage_length);
 void templateDoNotIdentified(__uint16_t TemplateID);
 void test();
  
@@ -15,12 +16,52 @@ int main () {
     return 0;
 }
 
+void MDIncRefresh_145(__uint8_t* FASTMessage, unsigned int FASTMessage_length){
+	__uint8_t field[7000];
+	unsigned int field_length = 0;
+	unsigned int noCurrentField = 0;
+
+	__uint32_t MsgSeqNum = 0;
+	__uint64_t SendintTime = 0;
+	__uint32_t TradeDate = 0;
+
+
+	printf(" TemplateID: 145 || Template name=MDIncRefresh_145 \n");
+	for(int i = 0; i < FASTMessage_length; i++){
+    	field[field_length] = FASTMessage[i];
+    	field_length++;
+
+    	if((field[field_length-1] >> 7) & 0b00000001){
+    		noCurrentField++;
+    		if(noCurrentField == 3){ //&& (pmap >><< fieldOrder)
+				MsgSeqNum = byteDecoder32(field, field_length);
+				printf(" MsgSeqNum: %d \n", MsgSeqNum);
+			}
+			else if(noCurrentField == 4){
+				printf(" SendingTime: ");
+				for(int i=0; i < field_length; i++){
+					printf("%02x ", (unsigned int) field[i]); //%u to a series of bytes while(*field){printf("%02x ", (unsigned int) *field++); // cast the character to an unsigned type to be safe
+				}
+				printf("\n");
+			}
+			else if(!(noCurrentField == 0 || noCurrentField == 1 || noCurrentField == 2)){
+				printf(" Field number %d do not identified: ", noCurrentField);
+				for(int i=0; i < field_length; i++){
+					printf("%02x ", (unsigned int) field[i]); //%u to a series of bytes while(*field){printf("%02x ", (unsigned int) *field++); // cast the character to an unsigned type to be safe
+				}
+				printf("\n");
+			}
+			field_length = 0;
+    	}
+    }
+}
+
 void MDHeartbeat_144(__uint8_t* FASTMessage, unsigned int FASTMessage_length){
 	__uint8_t field[7000];
 	unsigned int field_length = 0;
+	unsigned int noCurrentField = 0;
 	__uint32_t MsgSeqNum = 0;
 	__uint64_t SendingTime = 0;
-	unsigned int noCurrentField = 0;
 
 	printf(" TemplateID: 144 || Template name=MDHeartbeat_144 \n");
 	for(int i = 0; i < FASTMessage_length; i++){
@@ -40,7 +81,7 @@ void MDHeartbeat_144(__uint8_t* FASTMessage, unsigned int FASTMessage_length){
 				}
 				printf("\n");
 			}
-			else if(!noCurrentField == 0 && !noCurrentField == 1 && !noCurrentField == 2){
+			else if(!(noCurrentField == 0 || noCurrentField == 1 || noCurrentField == 2)){
 				printf(" Field number %d do not identified: ", noCurrentField);
 				for(int i=0; i < field_length; i++){
 					printf("%02x ", (unsigned int) field[i]); //%u to a series of bytes while(*field){printf("%02x ", (unsigned int) *field++); // cast the character to an unsigned type to be safe
@@ -56,6 +97,9 @@ void templateDecoder(__uint16_t TemplateID, __uint8_t* FASTMessage, unsigned int
 	switch(TemplateID)
 	{
 		case 144 : MDHeartbeat_144(FASTMessage, FASTMessage_length);
+		break;
+
+		case 145: MDIncRefresh_145(FASTMessage, FASTMessage_length);
 		break;
 
 		default : templateDoNotIdentified(TemplateID);
@@ -105,7 +149,7 @@ void readMessage(FILE* file){
 	int MsgLength = 0;
 
 	//while(fread(&byte, 1, 1, file) > 0){
-	for(int i = 0; i < 1; i++){ // number of messages
+	for(int i = 0; i < 1171; i++){ // number of messages
 		printf(" Message %d:    ----------------------------------------------------------\n", i+1);
 		for(int i = 0; i < 10; i++){ //read header
 			fread(&byte, 1, 1, file);
